ed44ba70149071bbb28144a3bf8112fc02d95542
tofu2220 <66412548+tofu2220@users.noreply.github.com>
2026-09-07 17:23:51 +0700

    feat: configure Fcitx5 US and Unikey input methods

--- home/tofu/desktop/fcitx5.nix
+++ home/tofu/desktop/fcitx5.nix
@@ -8,6 +8,27 @@
     fcitx5 = {
       addons = [ pkgs.qt6Packages.fcitx5-unikey ];
       waylandFrontend = true;
+
+      settings = {
+        globalOptions = {
+          Hotkey.AltTriggerKeys = "";
+
+          "Hotkey/TriggerKeys"."0" = "Alt+Shift+Shift_L";
+        };
+
+        inputMethod = {
+          "GroupOrder"."0" = "Default";
+
+          "Groups/0" = {
+            Name = "Default";
+            "Default Layout" = "us";
+            DefaultIM = "keyboard-us";
+          };
+
+          "Groups/0/Items/0".Name = "keyboard-us";
+          "Groups/0/Items/1".Name = "unikey";
+        };
+      };
     };
   };
 }
