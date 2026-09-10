766b21691541a81d1575704a61e76c90c5c9a1bf
tofu2220 <66412548+tofu2220@users.noreply.github.com>
2026-09-10 15:02:48 +0700

    Hardware configuration

--- hosts/t14/hardware-configuration.nix
+++ hosts/t14/hardware-configuration.nix
@@ -8,36 +8,36 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usbhid" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/9e9b62e8-67fc-476e-9c63-82589713904c";
+    { device = "/dev/disk/by-uuid/61c8e7dd-3f05-4d03-aade-de0cf565bc81";
       fsType = "btrfs";
     };
 
   fileSystems."/nix" =
-    { device = "/dev/disk/by-uuid/9e9b62e8-67fc-476e-9c63-82589713904c";
+    { device = "/dev/disk/by-uuid/61c8e7dd-3f05-4d03-aade-de0cf565bc81";
       fsType = "btrfs";
       options = [ "subvol=nix" ];
     };
 
   fileSystems."/home" =
-    { device = "/dev/disk/by-uuid/9e9b62e8-67fc-476e-9c63-82589713904c";
+    { device = "/dev/disk/by-uuid/61c8e7dd-3f05-4d03-aade-de0cf565bc81";
       fsType = "btrfs";
       options = [ "subvol=home" ];
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/B0B9-ED43";
+    { device = "/dev/disk/by-uuid/9127-DAF6";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/16683973-7704-4209-89ac-bad424b41be0"; }
+    [ { device = "/dev/disk/by-uuid/1a58ef00-7e82-4a63-bbfe-4b55eaf021cb"; }
     ];
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
