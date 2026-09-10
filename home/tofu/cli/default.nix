f7368c372a64db879a499aea9c1e8d43b8add324
tofu2220 <66412548+tofu2220@users.noreply.github.com>
2026-09-10 20:54:28 +0700

    fix(ssh): preserve terminal true-color settings

--- home/tofu/cli/default.nix
+++ home/tofu/cli/default.nix
@@ -3,7 +3,7 @@
     ./git.nix
     ./helix.nix
     ./shell.nix
-    ./ssh.nix
+    # ./ssh.nix
     ./tools.nix
   ];
 }
--- modules/services/ssh.nix
+++ modules/services/ssh.nix
@@ -1,8 +1,17 @@
 { ... }:
 
 {
+  # SSH client: gửi khả năng true-color của terminal hiện tại.
+  programs.ssh.extraConfig = ''
+    Host *
+      SendEnv COLORTERM
+  '';
+
+  # SSH server: chấp nhận biến đó trong phiên SSH.
   services.openssh = {
     enable = true;
     openFirewall = true;
+
+    settings.AcceptEnv = [ "COLORTERM" ];
   };
 }
