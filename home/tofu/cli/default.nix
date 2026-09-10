9f5df2baafa3fdb319c03c54b9a9eea2f4aafe1d
tofu2220 <66412548+tofu2220@users.noreply.github.com>
2026-09-10 20:58:18 +0700

    chore(home): remove unused SSH client config

--- home/tofu/cli/default.nix
+++ home/tofu/cli/default.nix
@@ -3,7 +3,6 @@
     ./git.nix
     ./helix.nix
     ./shell.nix
-    # ./ssh.nix
     ./tools.nix
   ];
 }
--- home/tofu/cli/ssh.nix
+++
@@ -1,8 +0,0 @@
-{ ... }:
-
-{
-  programs.ssh = {
-    enable = true;
-    enableDefaultConfig = false;
-  };
-}
